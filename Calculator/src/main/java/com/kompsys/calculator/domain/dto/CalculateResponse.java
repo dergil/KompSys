23f01d356561410cdf1d8f885183aa35d8ac0cb8
dergil <cpconrad@posteo.de>
2021-12-20 19:37:30 +0100

    calculator unit tested

--- Calculator/src/main/java/com/kompsys/calculator/domain/dto/CalculateResponse.java
+++ Calculator/src/main/java/com/kompsys/calculator/domain/dto/CalculateResponse.java
@@ -1,8 +1,12 @@
 package com.kompsys.calculator.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CalculateResponse {
     double priceTotal;
     double price;
