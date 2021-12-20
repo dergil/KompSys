d8c2f7773d7107c92378cc161eaed0ccb2a2744c
dergil <cpconrad@posteo.de>
2021-12-20 14:19:32 +0100

    swagger ui

--- KompSys/src/main/java/com/kbe/kompsys/configuration/SpringFoxConfig.java
+++ KompSys/src/main/java/com/kbe/kompsys/configuration/SpringFoxConfig.java
@@ -6,15 +6,15 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {
+//    http://localhost:8081/swagger-ui.html
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.kbe.kompsys")).build();
     }
 }
