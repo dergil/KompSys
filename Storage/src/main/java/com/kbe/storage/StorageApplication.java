79b4edabd7f338a6941d79e98c85e5d16b68cacf
PhilippDahlke <Philipp_dahlke@gmx.de>
2021-12-21 17:35:59 +0100

    + Storage Spring initializr

---
+++ Storage/src/main/java/com/kbe/storage/StorageApplication.java
@@ -0,0 +1,13 @@
+package com.kbe.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApplication.class, args);
+    }
+
+}
