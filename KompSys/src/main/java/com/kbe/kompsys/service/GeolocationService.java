b39d6b087b47bc618a58ee6f3ed3745c7b7219c7
dergil <cpconrad@posteo.de>
2022-03-01 20:36:24 +0100

    added weblux dependency for geolocation service

--- KompSys/src/main/java/com/kbe/kompsys/service/GeolocationService.java
+++ KompSys/src/main/java/com/kbe/kompsys/service/GeolocationService.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dergil.kompsys.dto.geolocation.GeolocationResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 //import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.InetAddress;
@@ -15,15 +16,14 @@ import java.net.UnknownHostException;
 public class GeolocationService {
 
     GeolocationResponse getGeolocation(String ipAddr) throws JsonProcessingException, UnknownHostException {
-//        InetAddress inetAddress = validateIP(ipAddr);
-//        WebClient client = WebClient.create();
-//        String response = client.get()
-//                .uri("http://ip-api.com/json/" + inetAddress.getHostAddress())
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
-//        return extractGeolocationResponse(response);
-        return null;
+        InetAddress inetAddress = validateIP(ipAddr);
+        WebClient client = WebClient.create();
+        String response = client.get()
+                .uri("http://ip-api.com/json/" + inetAddress.getHostAddress())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return extractGeolocationResponse(response);
     }
 
     private InetAddress validateIP(String ipAddr) throws UnknownHostException {
