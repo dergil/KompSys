ab904a04d80519c8f12abada1a1ef7f6d5ef57d3
PhilippDahlke <Philipp_dahlke@gmx.de>
2022-03-19 17:06:20 +0100

    + changed source of tax to request by RabbitMQ

--- KompSys/src/main/java/com/kbe/kompsys/service/TaxServiceMapper.java
+++ KompSys/src/main/java/com/kbe/kompsys/service/TaxServiceMapper.java
@@ -2,11 +2,10 @@ package com.kbe.kompsys.service;
 
 import com.github.dergil.kompsys.dto.calculate.CalculateResponse;
 import com.github.dergil.kompsys.dto.car.tax.CarTaxCalculateView;
+import com.github.dergil.kompsys.dto.tax.TaxView;
 import com.kbe.kompsys.domain.mapper.CalculateViewMapper;
 import com.kbe.kompsys.domain.mapper.CarViewMapper;
-import com.kbe.kompsys.domain.mapper.TaxViewMapper;
 import com.kbe.kompsys.domain.model.Car;
-import com.kbe.kompsys.domain.model.Tax;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +14,13 @@ public class TaxServiceMapper {
     @Autowired
     private CarViewMapper carViewMapper;
     @Autowired
-    private TaxViewMapper taxViewMapper;
-    @Autowired
     private CalculateViewMapper calculateViewMapper;
 
-    public CarTaxCalculateView mapToCarTaxCalculateView(Car car, CalculateResponse calculateResponse, Tax tax){
+    public CarTaxCalculateView mapToCarTaxCalculateView(Car car, CalculateResponse calculateResponse, TaxView tax) {
         CarTaxCalculateView response = new CarTaxCalculateView();
         response.setCarView(carViewMapper.toCarView(car));
         response.setCalculateView(calculateViewMapper.toCalculateView(calculateResponse));
-        response.setTaxView(taxViewMapper.toTaxView(tax));
+        response.setTaxView(tax);
         return response;
     }
 
